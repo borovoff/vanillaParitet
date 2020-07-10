05528180a9491dfe1b4ae1e64cbe9a50822f7345
borovoff <borovov.alexandr@gmail.com>
2020-07-10 22:11:48 +0300

    init

---
+++ app.js
@@ -0,0 +1,44 @@
+const wrap = document.createElement('div')
+wrap.classList.add('wrap')
+document.body.appendChild(wrap)
+
+const content = document.createElement('div')
+
+async function fetchHtmlAsText(url) {
+    return await (await fetch(url)).text();
+}
+
+async function changePage(pathname) {
+    content.innerHTML = await fetchHtmlAsText((pathname === '/' ? 'main' : pathname) + '.html')
+}
+
+async function loadHome(pathname) {
+    const footer = document.createElement('footer')
+    footer.innerHTML = await fetchHtmlAsText('footer.html')
+    document.body.appendChild(footer)
+
+    const nav = document.createElement('nav')
+    const cs = ['navbar', 'navbar-expand-lg', 'navbar-light']
+    cs.forEach(c => nav.classList.add(c))
+    nav.innerHTML = await fetchHtmlAsText('nav.html')
+    wrap.appendChild(nav)
+    wrap.appendChild(content)
+
+    await changePage(pathname)
+}
+
+loadHome(location.pathname)
+
+const onNavigate = async (pathname) => {
+    history.pushState(
+        {},
+        pathname,
+        location.origin + pathname
+    )
+
+    await changePage(pathname)
+}
+
+onpopstate = () => {
+    console.log('state poped')
+}
